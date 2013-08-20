0f89b7735da8bebed97fdeba4e79cd23acf514fc
Simon Rotter <simon.rotter@fu-berlin.de>
2013-08-20 16:10:35 +0200

    adding hello world class and adding files

---
+++ test_java_project/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Simon
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO GIT HUB!");
+
+	}
+
+}
